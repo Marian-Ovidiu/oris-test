917bcbc4887d6ba2d6a20da5eda2747557f6717c
Marian-Ovidiu Hutanu <h.marian914@gmail.com>
2023-10-23 20:11:32 +0200

    Products Page filter WIP

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faBars, faEnvelope, faMapMarker, faPhone  } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faEnvelope, faMapMarker, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBars, faEnvelope, faMapMarker, faPhone, faInstagram, faFacebook);
+library.add(faBars, faEnvelope, faMapMarker, faPhone, faInstagram, faFacebook, faWindowClose);
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-12 filter-container">
                 <div class="filter" v-for="(filter, index) in filters" :key="index" :class="{ 'selected': filter.selected }"
-                    @click="toggleFilter(filter)">
+                    @click="toggleMainFilter(filter)">
                     {{ filter.label }}
                 </div>
             </div>
@@ -24,8 +24,37 @@
     <Footer></Footer>
     <div id="filter-popup" class="filter-popup" v-if="isPopupOpen">
         <div class="popup-content">
-            <span class="close" @click="closePopup">×</span>
-            <h2>{{ selectedFilter.label }}</h2>
+            <div class="header-popup">
+                <div class="header-title-popup">
+                    <span>{{ selectedFilter.label }}</span>
+                </div>
+                <div class="header-close-popup">
+                    <font-awesome-icon icon="fa-window-close" class="fa-2x fa-brand close" @click="closePopup" />
+                </div>
+            </div>
+
+            <div class="container-fluid">
+                <div class="content-popup row">
+                    <div class="col-12 col-sm-12 col-md-6 col-xl-6 col-xxl-6"
+                        v-for="(filter, index) in selectedFilter.filterData" :key="index">
+                        <div class="inner-filter" @click="toggleInnerFilter(filter, selectedFilter.id)">
+                            {{ filter.name }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container-fluid">
+                <div class="footer-popup row">
+                    <div class="col-12 col-sm-6 col-md-6 col-xl-6 col-xxl-6 footer-save-popup">
+                        <span>Salva</span>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-6 col-xl-6 col-xxl-6 footer-close-popup" @click="closePopup">
+                        <span>Chiudi</span>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -50,23 +79,94 @@ export default {
             isPopupOpen: false,
             selectedFilter: null,
             filters: [
-                { label: 'Filtro 1', selected: false },
-                { label: 'Filtro 2', selected: false },
-                { label: 'Filtro 3', selected: false },
+                {
+                    id: 1,
+                    label: 'Brand',
+                    selected: false,
+                    filterData: [
+                        {
+                            id: 1,
+                            name: 'Tommy Hilfiger'
+                        },
+                        {
+                            id: 2,
+                            name: 'Polo Ralph Lauren'
+                        },
+                        {
+                            id: 3,
+                            name: 'Hugo Boss'
+                        }
+                    ]
+                },
+                {
+                    id: 2,
+                    label: 'Taglie',
+                    selected: false,
+                    filterData: [
+                        {
+                            id: 1,
+                            name: '32'
+                        },
+                        {
+                            id: 2,
+                            name: '33'
+                        },
+                        {
+                            id: 3,
+                            name: '34'
+                        },
+                        {
+                            id: 4,
+                            name: '35'
+                        },
+                        {
+                            id: 5,
+                            name: '36'
+                        },
+                        {
+                            id: 6,
+                            name: '37'
+                        }
+                    ]
+                }
             ],
+            selectedFilters: []
         };
     },
     methods: {
-        toggleFilter(filter) {
-
-            if (!filter.selected) {
-                this.isPopupOpen = true;
-            }
-
+        toggleMainFilter(filter) {
             filter.selected = !filter.selected;
 
             this.selectedFilter = filter;
+            this.isPopupOpen = true;
 
+        },
+        toggleInnerFilter(innerFilter, idFilter) {
+            let indexParentExist = this.selectedFilters.findIndex(obj => obj.parentId === idFilter);
+
+            if (indexParentExist === -1) {
+                this.selectedFilters.push({
+                    parentId: idFilter,
+                    childFilters: [
+                        innerFilter
+                    ]
+                });
+
+            } else {
+                let indexChildExist = this.selectedFilters[indexParentExist].childFilters.findIndex(obj => obj.id === innerFilter.id);
+
+                if (indexChildExist === -1) {
+                    this.selectedFilters[indexParentExist].childFilters.push(innerFilter);
+                } else {
+                    this.selectedFilters[indexParentExist].childFilters.splice(indexChildExist, 1);
+                }
+            }
+
+            if (indexParentExist !== -1) {
+                if (this.selectedFilters[indexParentExist].childFilters.length <= 0) {
+                    this.selectedFilters.splice(indexParentExist, 1);
+                }
+            }
         },
         closePopup() {
             this.isPopupOpen = false;
@@ -76,6 +176,15 @@ export default {
 </script>
 
 <style scoped>
+#filter-popup.container-fluid:first-child {
+    max-height: 350px !important;
+    overflow: scroll !important;
+}
+
+#filter-popup.container-fluid:first-child::-webkit-scrollbar {
+    display: none !important;
+}
+
 .selected {
     background-color: gray;
     color: white;
@@ -123,7 +232,6 @@ export default {
 .single-filter-details-container {
     width: 100vw;
     height: 100vh;
-    background-color: red;
     z-index: 9999;
     position: absolute;
 
@@ -139,14 +247,81 @@ export default {
 }
 
 .popup-content {
+    width: 50%;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     background-color: #fff;
-    padding: 20px;
     border-radius: 5px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
     text-align: center;
+}
+
+.header-popup {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    background-color: lightgrey;
+}
+
+.footer-popup {
+    height: 50px;
+    display: flex;
+    background-color: lightgrey;
+}
+
+.content-popup {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 10px 30px;
+}
+
+.inner-filter {
+    height: 45px;
+    border: 1px solid gray;
+    margin: 5px 0;
+    text-align: center;
+    line-height: 45px;
+}
+
+.header-title-popup {
+    width: 75%;
+}
+
+.footer-save-popup {
+    text-align: center;
+    line-height: 50px;
+    background-color: green;
+}
+
+.header-close-popup {
+    width: 25%;
+    flex-direction: row-reverse;
+}
+
+.footer-close-popup {
+    flex-direction: row-reverse;
+    text-align: center;
+    line-height: 50px;
+}
+
+@media (max-width: 576px) {
+    .footer-popup {
+        height: 70px;
+    }
+
+    .footer-save-popup,
+    .footer-close-popup {
+        line-height: 35px;
+    }
+}
+
+.header-title-popup,
+.header-close-popup {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
 }
 </style>
